895265c94506edb606ca2e70d6905209d3d86fcc
Will Refvem <wbrefvem@ncsu.edu>
2014-01-16 18:16:47 -0500

    Tasking debug

--- tasks.py
+++ tasks.py
@@ -200,7 +200,7 @@ def save_reminder_record(user,id, date):
 # Crontab in settings.py
 @task
 def send_reminder_emails():
-    send_emails = True
+    send_emails = settings.WORKLOG_SEND_REMINDERS
     if send_emails:
         datatuples = ()  # one tuple for each email to send... contains subj, msg, recipients, etc...
         date = datetime.date.today()
