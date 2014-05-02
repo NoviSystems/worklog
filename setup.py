595ec379ac310ae8602b2f9401be450d19ab6656
Ryan P Kilby <rpkilby@ncsu.edu>
2014-05-02 13:42:06 -0400

    Moved worklog app into app directory

--- setup.py
+++ setup.py
@@ -4,23 +4,27 @@ from setuptools import setup
 def read(fname):
   return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name = "Worklog",
-          version = "0.8",
-          author = "Brian Bouterse",
-          author_email = "bmbouter@gmail.com",
-          description = ("A Django based, hourly work logging app which supports job coding, view filtering, csv report generation and an e-mail reminder system"),
-          keywords = "Work logging, Report, Email reminder",
-	  url = "https://www.fi.ncsu.edu/",
-	  long_description=read('README.md'),
-          classifiers=[ "Development Status :: 4 - Beta","Topic :: Utilities","License :: OSI Approved :: BSD License", 
-          ],
-          install_requires=[
-                            "django>=1.4.1",
-                            "celery>=3.0.9",
-                            "django-celery>=3.0.9",
-                            "xhtml2pdf",
-                            "pisa",
-                            "south",
-                            "pytz",
-                           ],
+setup(
+    name = "Worklog",
+    version = "0.8",
+    author = "Brian Bouterse",
+    author_email = "bmbouter@gmail.com",
+    description = ("A Django based, hourly work logging app which supports job coding, view filtering, csv report generation and an e-mail reminder system"),
+    keywords = "Work logging, Report, Email reminder",
+    url = "https://www.fi.ncsu.edu/",
+    long_description=read('README.md'),
+    packages = ['worklog'],  
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities","License :: OSI Approved :: BSD License", 
+    ],
+    install_requires=[
+        "django>=1.4.1",
+        "celery>=3.0.9",
+        "django-celery>=3.0.9",
+        "xhtml2pdf",
+        "pisa",
+        "south",
+        "pytz",
+    ],
 )
