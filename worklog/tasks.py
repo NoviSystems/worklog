c8cf74513573a8f4736fea095320824c88faf7e4
Will Refvem <wbrefvem@ncsu.edu>
2014-05-12 17:57:56 -0400

    Fixed tasking bug

--- worklog/tasks.py
+++ worklog/tasks.py
@@ -191,7 +191,7 @@ def compose_reminder_email(email_address, id, date):
 
 
 def create_reminder_url(id):
-    path = urlreverse('worklog-today', kwargs={'date': 'today'}, current_app='worklog')
+    path = urlreverse('worklog-current-date', current_app='worklog')
     return settings.SITE_URL + path
 
 
