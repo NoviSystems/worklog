90af8acdd886172e547371aba8bd64b34a43e597
dralley <dralley@ncsu.edu>
2014-07-14 13:15:29 -0400

    style changes for better PEP conformance

--- worklog/admin_filter.py
+++ worklog/admin_filter.py
@@ -8,38 +8,39 @@ from django.contrib.auth.models import User
 import datetime
 import operator
 
+
 # used in YearMonthFilterSpec.__init__
-def create_yearmonth_link(d,fieldname):
+def create_yearmonth_link(d, fieldname):
     """d = a datetime.date object """
     title = smart_unicode(d.strftime('%Y %B'))
-    param_dict = { 
-            '%s__year' % fieldname: str(d.year), 
-            '%s__month' % fieldname: str(d.month), 
-            }
-    return title,param_dict
+    param_dict = {
+            '%s__year' % fieldname: str(d.year),
+            '%s__month' % fieldname: str(d.month),
+        }
+    return title, param_dict
+
 
 class YearMonthFilterSpec(SimpleListFilter):
     def __init__(self, f, request, params, model, *args, **kwargs):
-        super(YearMonthFilterSpec,self).__init__(f,request,params,model,*args,**kwargs)
-        
+        super(YearMonthFilterSpec, self).__init__(f, request, params, model, *args, **kwargs)
+
         self.field_generic = '%s__' % self.field.name
         self.date_params = dict([(k, v) for k, v in params.items() if k.startswith(self.field_generic)])
-        
+
         values_list = model.objects.values_list(f.name, flat=True)
         unique_dates = list(set(
             val.replace(day=1) for val in values_list if isinstance(val, datetime.date)
             ))
         # using reverse=True results in most recent date being listed first
         unique_dates.sort(reverse=True)
-        self.links = list(create_yearmonth_link(d,self.field.name) for d in unique_dates)
-        self.links.insert(0,(_('All'),{}))
-        
+        self.links = list(create_yearmonth_link(d, self.field.name) for d in unique_dates)
+        self.links.insert(0, (_('All'), {}))
 
     def choices(self, cl):
         for title, param_dict in self.links:
             yield {'selected': self.date_params == param_dict,
                    'query_string': cl.get_query_string(param_dict, [self.field_generic]),
-                   'display': title } 
+                   'display': title}
 
     def title(self):
         return _('year and month')
@@ -50,7 +51,7 @@ class UserFilterSpec(SimpleListFilter):
         super(UserFilterSpec, self).__init__(f, request, params, model, *args, **kwargs)
 
         values_list = model.objects.values_list(f.name, flat=True)
-        
+
         self.lookup_choices = list((x.pk,x.username) for x in User.objects.all())
         self.lookup_choices.sort(key=operator.itemgetter(0))
 
@@ -58,10 +59,11 @@ class UserFilterSpec(SimpleListFilter):
         yield {'selected': self.lookup_val is None,
                'query_string': cl.get_query_string({}, [self.lookup_kwarg]),
                'display': _('All')}
-        for id,name in self.lookup_choices:
+        for id, name in self.lookup_choices:
             yield {'selected': smart_unicode(id) == self.lookup_val,
                    'query_string': cl.get_query_string({self.lookup_kwarg: id}),
                    'display': smart_unicode(name)}
+
 
 class IsInvoicedFilterSpec(SimpleListFilter):
     """
@@ -74,10 +76,9 @@ class IsInvoicedFilterSpec(SimpleListFilter):
         super(IsInvoicedFilterSpec, self).__init__(f, request, params, model, *args, **kwargs)
         self.links = (
             (_('Any'), {}),
-            (_('Not Invoiced'), {'%s__exact' % self.field.name: "0",}),
-            (_('Invoiced'), {'%s__exact' % self.field.name: "1",}),
+            (_('Not Invoiced'), {'%s__exact' % self.field.name: "0", }),
+            (_('Invoiced'), {'%s__exact' % self.field.name: "1", }),
         )
 
     def title(self):
         return "Invoiced"
-
