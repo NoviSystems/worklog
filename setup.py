81ff58148b020521cf8daa66fb27b21b1a8d2ada
Dylan Stein <djstein@ncsu.edu>
2015-09-08 14:20:14 -0400

    Edited setup.py, added .egg to gitignore

--- setup.py
+++ setup.py
@@ -1,5 +1,35 @@
 import os
 from setuptools import setup, find_packages
+from setuptools.command.test import test
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+class Test(test):
+    user_options = [
+        ('test-labels=', 'l', "Test labels to pass to runner.py test"),
+        ('djtest-args=', 'a', "Arguments to pass to runner.py test"),
+    ]
+
+    def initialize_options(self):
+        test.initialize_options(self)
+        self.test_labels = 'tests'
+        self.djtest_args = ''
+
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        from tests.runner import main
+
+        test_labels = self.test_labels.split()
+        djtest_args = self.djtest_args.split()
+        main(['runner.py', 'test'] + test_labels + djtest_args)
 
 
 def read(fname):
@@ -10,6 +40,7 @@ setup(
     version="0.8",
     author="Brian Bouterse",
     author_email="bmbouter@gmail.com",
+
     description=("A Django based, hourly work logging app which supports job coding, view filtering, csv report generation and an e-mail reminder system"),
     keywords="Work logging, Report, Email reminder",
     url="https://www.fi.ncsu.edu/",
@@ -20,6 +51,7 @@ setup(
         "Development Status :: 4 - Beta",
         "Topic :: Utilities", "License :: OSI Approved :: BSD License",
     ],
+
     install_requires=[
         "celery",
         "djangorestframework",
@@ -30,4 +62,10 @@ setup(
         "pisa",
         "pytz",
     ],
+
+    cmdclass={
+        'test': Test,
+    },
+
+    tests_require=['django', 'djangorestframework>=3.2,!=3.2.3', 'fantasy-database'],
 )
