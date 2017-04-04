b5bc5bb5894f6624038429ea17906b45b5721a70
Ryan P Kilby <rpkilby@ncsu.edu>
2017-04-04 19:52:37 -0400

    Cleanup viewwork templates

--- worklog/templatetags/worklog_tags.py
+++
@@ -1,7 +0,0 @@
-from django import template
-register = template.Library()
-
-
-@register.filter
-def verbose_name(model_set, field):
-    return model_set[0]._meta.get_field(field).verbose_name.title()
