f84fbe7ce1a1c999db0e01133b03b644a4f9b6af
Ryan P Kilby <rpkilby@ncsu.edu>
2017-04-04 18:15:17 -0400

    Remove old date filter & dead code

--- worklog/templatetags/worklog_tags.py
+++ worklog/templatetags/worklog_tags.py
@@ -6,6 +6,7 @@ register = template.Library()
 #       django/contrib/admin/templatetags/admin_list.py
 # This is nearly identical to the cited code.  This is used by our overridden
 # admin template.
+@register.inclusion_tag('admin/worklog/workitem/actions.html', takes_context=True)
 def workitem_admin_actions(context):
     """
     Track the number of times the action field has been rendered on the page,
@@ -14,7 +15,6 @@ def workitem_admin_actions(context):
     context['action_index'] = context.get('action_index', -1) + 1
     return context
 
-workitem_admin_actions = register.inclusion_tag("admin/worklog/workitem/actions.html", takes_context=True)(workitem_admin_actions)
 
 @register.filter
 def verbose_name(model_set, field):
